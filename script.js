386acf5f013bd68f5b220664b50e69039edfdac2
ayeshatariq12 <50871466+ayeshatariq12@users.noreply.github.com>
2019-07-11 02:24:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let whatsapp = {
+    contacts: [{
+        name: "Ammara",
+        profilePic: "Image",
+        message: "hello",
+        time: "9:45 Am",
+        contact: "0333-7896548",
+    }]
+}
+
+let techKaro = {
+    name: "tech karo",
+    about: "web dev course",
+    students: [{
+        name: "Areeba",
+        dateOfBirth: "03-02-1998",
+        age: "21",
+        height: "5",
+        group: "B",
+        phoneNumber: "03378454612",
+        currentAdress: "gulshan-e-iqbal,karachi"
+    }]
+
+        [{
+        name: "Ramsha",
+        dateOfBirth: "07-05-1998",
+        age: "21",
+        height: "5.5",
+        group: "B",
+        phoneNumber: "03378454612",
+        currentAdress: "gulstan-e-jahur,karachi",
+    }]
+}
